00d181985f76e88488458e937c2d56646b598f89
meemmahmuda <meemmahmuda85@gmail.com>
2024-09-16 18:35:41 +0600

    up

--- front/src/components/Expenses/ExpenseList.jsx
+++ front/src/components/Expenses/ExpenseList.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Lin } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ExpenseList = () => {
   const [expenses, setExpenses] = useState([]);
